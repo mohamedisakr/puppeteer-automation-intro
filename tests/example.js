a4cc795eaa7b8e695f3e74454f75c91ea4c79c45
mohamedisakr <mohamedisakr@gmail.com>
2021-02-18 17:47:09 +0200

    using reload function

--- tests/example.js
+++ tests/example.js
@@ -9,10 +9,22 @@ describe("my first test", () => {
     await browser.close();
   });
 
-  it.only("should wait for 3 seconds", async () => {
+  it("should wait for 3 seconds", async () => {
     const browser = await puppeteer.launch(); //{ headless: false }
     const page = await browser.newPage();
     await page.goto("http://example.com");
+    await page.waitForTimeout(300);
+    await page.waitForSelector("h1");
+    await browser.close();
+  });
+
+  it.only("should reload the page", async () => {
+    const browser = await puppeteer.launch(); //{ headless: false }
+    const page = await browser.newPage();
+    await page.goto("http://example.com");
+    await page.waitForTimeout(300);
+    await page.waitForSelector("h1");
+    await page.reload();
     await page.waitForTimeout(300);
     await page.waitForSelector("h1");
     await browser.close();
