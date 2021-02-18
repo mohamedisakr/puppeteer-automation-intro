393eb6daa39ebf6c9e7f5f1f332379a5509b9cfb
mohamedisakr <mohamedisakr@gmail.com>
2021-02-18 17:37:34 +0200

    using page waitfor

--- tests/example.js
+++ tests/example.js
@@ -8,4 +8,13 @@ describe("my first test", () => {
     await page.waitForSelector("h1");
     await browser.close();
   });
+
+  it.only("should wait for 3 seconds", async () => {
+    const browser = await puppeteer.launch(); //{ headless: false }
+    const page = await browser.newPage();
+    await page.goto("http://example.com");
+    await page.waitForTimeout(300);
+    await page.waitForSelector("h1");
+    await browser.close();
+  });
 });
